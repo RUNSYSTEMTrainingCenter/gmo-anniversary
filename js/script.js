c20dbc392f110d79741f8c8dc959d2cf84212f3c
ThienLV <ohmygodvt95@gmail.com>
2018-04-19 14:45:16 +0700

    preview

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 angular.module("App", []).controller('Image', function ($scope) {
 
+    $scope.name = "Nguyễn Văn A";
+    $scope.year = 5;
+    $scope.branch = "Ha Noi Branch";
+    $scope.position = "iOS Developer";
+
+    $scope.getYear = function (y) {
+      var d = new Date();
+      $scope.year = d.getFullYear() - parseInt(y.split("/")[2]);
+      console.log(d);
+    };
+
+    $scope.nth = function(d) {
+        if(d>3 && d<21) return 'th'; // thanks kennebec
+        switch (d % 10) {
+            case 1:  return "st";
+            case 2:  return "nd";
+            case 3:  return "rd";
+            default: return "th";
+        }
+    };
+
     $scope.submit = function(e) {
         e.preventDefault();
         html2canvas(document.querySelector("#capture")).then(function (canvas) {
